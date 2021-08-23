e5ffa582fc34048fe5d7baf918b77a0f6339940d
Hendriyawan <hendrijs44@gmail.com>
2021-08-23 17:57:47 +0700

    Initial commit

---
+++ AdmobRemoteConfig/src/main/java/com/gdev/admobremoteconfig/utils/RemoteConfigUtils.kt
@@ -0,0 +1,62 @@
+package com.gdev.admobremoteconfig.utils
+
+import android.content.Context
+import android.util.Log.d
+import com.gdev.admobremoteconfig.BuildConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+
+object RemoteConfigUtils {
+    const val TAG = "RemoteConfigUtils"
+    const val AD_BANNER_ID = "ad_banner_id"
+    const val AD_INTERSTITIAL_ID = "ad_interstitial_id"
+    const val HAS_MOVE_PLAY_STORE = "has_move_playstore"
+    const val NEW_PLAY_STORE_URL = "new_play_store_url"
+    const val VERSION_NAME = "version_name"
+    const val VERSION_CODE = "version_code"
+    private var defaults = HashMap<String, Any>()
+    private var remoteConfig: FirebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+
+    /**
+     * build default values configuration
+     * @param context Context
+     * @return HashMap<String, Any>() return a data from fetch
+     */
+    fun buildDefaultValues(context: Context): HashMap<String, Any> {
+        return defaults.apply {
+            put("${context.packageName}-$AD_BANNER_ID", "ca-app-pub-3940256099942544/6300978111")
+            put("${context.packageName}-$AD_INTERSTITIAL_ID", "ca-app-pub-3940256099942544/1033173712")
+            put("${context.packageName}-$HAS_MOVE_PLAY_STORE", false)
+            put("${context.packageName}-$NEW_PLAY_STORE_URL", "https://play.google.com/store/apps/details?id=${context.packageName}")
+            put("${context.packageName}-$VERSION_NAME", "1.0")
+            put("${context.packageName}-$VERSION_CODE", 1)
+        }
+    }
+
+    /**
+     * fetch data configuration
+     */
+    fun getFirebaseRemoteConfig(
+        context: Context,
+        listener: (Boolean, HashMap<String, Any>) -> Unit
+    ) {
+        val remoteSettings = FirebaseRemoteConfigSettings.Builder().apply {
+            minimumFetchIntervalInSeconds = if (BuildConfig.DEBUG) 0 else 60 * 60
+        }.build()
+        remoteConfig.setConfigSettingsAsync(remoteSettings)
+        remoteConfig.setDefaultsAsync(buildDefaultValues(context))
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            val success = it.isSuccessful
+            if (success) {
+                //val updated = it.result
+                val fetched = HashMap<String, Any>()
+                fetched["${context.packageName}-$AD_BANNER_ID"] = remoteConfig.getString("${context.packageName}-$AD_BANNER_ID")
+                fetched["${context.packageName}-$AD_INTERSTITIAL_ID"] = remoteConfig.getString("${context.packageName}-$AD_INTERSTITIAL_ID")
+                fetched["${context.packageName}-$HAS_MOVE_PLAY_STORE"] = remoteConfig.getBoolean("${context.packageName}-$HAS_MOVE_PLAY_STORE")
+                fetched["${context.packageName}-$NEW_PLAY_STORE_URL"] = remoteConfig.getString("${context.packageName}-$NEW_PLAY_STORE_URL")
+                d(TAG, "$fetched")
+                listener(success, fetched)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/gdev/remote/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.gdev.remote
+
+import android.os.Bundle
+import android.util.Log.d
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
+import com.gdev.admobremoteconfig.utils.RemoteConfigUtils
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //fetch data from Config
+        //build default values first
+        RemoteConfigUtils.buildDefaultValues(this)
+        RemoteConfigUtils.getFirebaseRemoteConfig(this) { isSuccess, data ->
+            val adView = AdView(this)
+            adView.adSize = AdSize.LARGE_BANNER
+            adView.adUnitId = data["$packageName-${RemoteConfigUtils.AD_BANNER_ID}"].toString()
+            adView.loadAd(AdRequest.Builder().build())
+            findViewById<LinearLayoutCompat>(R.id.layout_ad_view).apply {
+                addView(adView)
+            }
+        }
+    }
+}
